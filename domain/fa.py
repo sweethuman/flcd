2f730bc4f4376b91f797a7525f003f2fcb79f2f5
sweethuman <gheo@sweethuman.tech>
2021-11-02 14:21:51 +0200

    add finite automata

---
+++ domain/fa.py
@@ -0,0 +1,90 @@
+from typing import List, Dict, Tuple
+
+
+class FiniteAutomata:
+    states: List[str]
+    alphabet: List[str]
+    transitions: Dict[Tuple[str, str], List[str]]
+    initial_state: str
+    final_states: List[str]
+
+    def __init__(self, states: List[str], alphabet: List[str], transitions: Dict[Tuple[str, str], List[str]],
+                 initial_state: str, final_states: List[str]):
+        self.states = states
+        self.alphabet = alphabet
+        self.transitions = transitions
+        self.initial_state = initial_state
+        self.final_states = final_states
+
+    @staticmethod
+    def readFromFile(file_name: str):
+        states = []
+        alphabet = []
+        transitions = {}
+        initial_state = None
+        final_states = []
+
+        with open(file_name, 'r') as file:
+            for lineCounter, line in enumerate(file, start=1):
+                line: str = line.strip()
+                tokens = line.split(";")
+                if tokens[0] == "states":
+                    states = tokens[1:]
+                elif tokens[0] == "alphabet":
+                    alphabet = tokens[1:]
+                elif tokens[0] == "transitions":
+                    for transition in tokens[1:]:
+                        transition_tokens = transition.split(",")
+                        trans = (transition_tokens[0], transition_tokens[1])
+                        if trans in transitions:
+                            transitions[trans].append(transition_tokens[2])
+                        else:
+                            transitions[trans] = [transition_tokens[2]]
+                elif tokens[0] == "initialState":
+                    initial_state = tokens[1]
+                elif tokens[0] == "finalState":
+                    final_states = tokens[1:]
+        return FiniteAutomata(states, alphabet, transitions, initial_state, final_states)
+
+    def printStates(self):
+        print("Set of states: ", self.states)
+
+    def printAlphabet(self):
+        print("FA alphabet: ", self.alphabet)
+
+    def printInitialState(self):
+        print("Initial state: ", self.initial_state)
+
+    def printFinalStates(self):
+        print("Final states: ", self.final_states)
+
+    def printTransitions(self):
+        print("Transitions: ")
+        for t in self.transitions.keys():
+            print("delta({0}, {1}) = {2}".format(t[0], t[1], self.transitions[t]))
+
+    def checkSequence(self, sequence) -> bool:
+        current_state = self.initial_state
+
+        while sequence != "":
+            transition_key = (current_state, sequence[0])
+
+            if transition_key in self.transitions.keys():
+                current_state = self.transitions[transition_key][0]
+                sequence = sequence[1:]
+
+            else:
+                return False
+
+        return current_state in self.final_states
+
+
+if __name__ == "__main__":
+    automata = FiniteAutomata.readFromFile("fa_test.in")
+    automata.printStates()
+    automata.printAlphabet()
+    automata.printInitialState()
+    automata.printFinalStates()
+    automata.printTransitions()
+    print("12345", automata.checkSequence("12345"))
+    print("123c45", automata.checkSequence("123c45"))
